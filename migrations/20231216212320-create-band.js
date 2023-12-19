b0135c4b533afa96a79985bac576840f3ad8a15a
Johnson Yang <johnsonyang65@gmail.com>
2023-12-18 20:18:55 -0800

    added controller and routes

--- migrations/20231216212320-create-band.js
+++ migrations/20231216212320-create-band.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('bands', {
+    await queryInterface.createTable('Bands', {
       band_id: {
         allowNull: false,
         autoIncrement: true,
